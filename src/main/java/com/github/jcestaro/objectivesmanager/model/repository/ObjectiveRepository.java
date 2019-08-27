4cba5ca7ae5f4c4551f948cf42104532bea518d9
josecestaro <joseaugusto.conceicao@bluesoft.com.br>
2019-08-27 15:12:39 -0300

    adding repository annotation to ObjectiveRepository

--- src/main/java/com/github/jcestaro/objectivesmanager/model/repository/ObjectiveRepository.java
+++ src/main/java/com/github/jcestaro/objectivesmanager/model/repository/ObjectiveRepository.java
@@ -3,7 +3,9 @@ package com.github.jcestaro.objectivesmanager.model.repository;
 import com.github.jcestaro.objectivesmanager.model.entity.Objective;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ObjectiveRepository extends JpaRepository<Objective, Integer> {
 
 }
